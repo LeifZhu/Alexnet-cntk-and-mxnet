1f02949a1f33366a37ec04197b6b0cc76d34fb1c
zhulei <leifzhu@foxmail.com>
2018-08-31 16:01:50 +0800

    deleted some files

--- mxnet-exp/kill_local.sh
+++ mxnet-exp/kill_local.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+
+ps aux | grep "monitor_mx_run.sh" | grep -v "grep" | awk {'print $2'} | xargs kill
 ps aux | grep "python code/train_imagenet.py" | grep -v "grep" | awk {'print $2'} | xargs kill
